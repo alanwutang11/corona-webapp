51cc0bdac01fcdf961fc099ace256dd70f68004c
Alan Wu <alpwu@ucdavis.edu>
2020-09-18 17:24:28 -0700

    Updates

---
+++ precache-manifest.c15ef2312523d3a0ada7111211923386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2b3eca6924af85bedc96846bd3a68f",
+    "url": "/corona-webapp/index.html"
+  },
+  {
+    "revision": "7c5c28b0d3d33edb3540",
+    "url": "/corona-webapp/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "87940868f3272075bb0c",
+    "url": "/corona-webapp/static/js/2.c0367fa5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/corona-webapp/static/js/2.c0367fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5c28b0d3d33edb3540",
+    "url": "/corona-webapp/static/js/main.bc5f6d93.chunk.js"
+  },
+  {
+    "revision": "f0409af87a3c0a4a9231",
+    "url": "/corona-webapp/static/js/runtime-main.b4546d10.js"
+  }
+]);
